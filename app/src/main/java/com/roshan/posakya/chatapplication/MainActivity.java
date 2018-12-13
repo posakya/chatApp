ffccc2ed0dc1c5321c0f6677974550ab94401d87
posakya <roshanposakya07@gmail.com>
2018-12-13 19:54:23 +0545

    third commit

--- app/src/main/java/com/roshan/posakya/chatapplication/MainActivity.java
+++ app/src/main/java/com/roshan/posakya/chatapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.roshan.posakya.chatapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -7,17 +8,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.text.format.DateFormat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.app.AlertDialog;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseListAdapter;
@@ -32,6 +40,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.roshan.posakya.chatapplication.progressDialog.ShowProgress;
+import com.scottyab.aescrypt.AESCrypt;
+
+
+import java.security.GeneralSecurityException;
 
 import hani.momanii.supernova_emoji_library.Actions.EmojIconActions;
 import hani.momanii.supernova_emoji_library.Helper.EmojiconEditText;
@@ -48,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     EmojiconEditText emojiconEditText;
     ImageView emojiButton,submitButton;
     EmojIconActions emojIconActions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,11 +81,20 @@ public class MainActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessage(emojiconEditText.getText().toString(),
-                        FirebaseAuth.getInstance().getCurrentUser().getEmail()));
-                emojiconEditText.setText("");
-                emojiconEditText.requestFocus();
-                adapter.notifyDataSetChanged();
+
+                try {
+                    String password = String.valueOf(R.string.app_name);
+                    String encryptedMsg = AESCrypt.encrypt(password,emojiconEditText.getText().toString());
+                    FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessage(encryptedMsg,
+                            FirebaseAuth.getInstance().getCurrentUser().getDisplayName()));
+
+                    emojiconEditText.setText("");
+                    emojiconEditText.requestFocus();
+                    adapter.notifyDataSetChanged();
+                }catch (GeneralSecurityException e){
+                    //handle error
+                }
+
             }
         });
 
@@ -111,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
     private void displayChatMessage() {
         progress.showProgress();
 
-        ListView listOfMessage = (ListView)findViewById(R.id.list_of_message);
+        final ListView listOfMessage = (ListView)findViewById(R.id.list_of_message);
 
         /// using query method ///
         final Query query = FirebaseDatabase.getInstance().getReference();
@@ -126,26 +148,105 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
 
+
+
         adapter = new FirebaseListAdapter<ChatMessage>(options)
         {
             @Override
-            protected void populateView(View v, ChatMessage model, int position) {
+            protected void populateView(View v, final ChatMessage model, final int position) {
                 progress.hideProgress();
-
                 //Get references to the views of list_item.xml
-                TextView messageText, messageUser, messageTime;
+                TextView messageText, messageUser, messageTime,message_text_other;
                 messageText = (BubbleTextView) v.findViewById(R.id.message_text);
+                message_text_other = (BubbleTextView) v.findViewById(R.id.message_text_other);
                 messageUser = (TextView) v.findViewById(R.id.message_user);
                 messageTime = (TextView) v.findViewById(R.id.message_time);
 
-                messageText.setText(model.getMessageText());
+                String messageAfterDecrypt = "";
+
+                try {
+                    String password = String.valueOf(R.string.app_name);
+                    messageAfterDecrypt = AESCrypt.decrypt(password, model.getMessageText());
+                    messageText.setText(messageAfterDecrypt);
+                    message_text_other.setText(messageAfterDecrypt);
+                }catch (GeneralSecurityException e){
+                    //handle error - could be due to incorrect password or tampered encryptedMsg
+                }
+
                 messageUser.setText(model.getMessageUser());
-                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)", model.getMessageTime()));
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:aa)", model.getMessageTime()));
+
+
+                String author = model.getMessageUser();
+                System.out.println("Current User : "+FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+                System.out.println("User : "+author);
+
+                if (author != null && author.equals(FirebaseAuth.getInstance().getCurrentUser().getDisplayName())){
+
+                    message_text_other.setVisibility(View.VISIBLE);
+                    messageText.setVisibility(View.GONE);
+
+                }else{
+                    message_text_other.setVisibility(View.GONE);
+                    messageText.setVisibility(View.VISIBLE);
+
+                    }
+
+
+                listOfMessage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
+
+
+                        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this,AlertDialog.THEME_HOLO_LIGHT).create();
+
+                        alertDialog.setTitle(R.string.app_name);
+                        alertDialog.setMessage(Html.fromHtml("Would you like to delete?"));
+
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "No",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        adapter.getRef(i).removeValue();
+                                        adapter.notifyDataSetChanged();
+                                        // Snackbar.make(activity_main,"We couldn't sign you in.Please try again later", Snackbar.LENGTH_SHORT).show();
+                                        Snackbar.make(activity_main, " Deleted",Snackbar.LENGTH_SHORT).show();
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                        alertDialog.show();
+                        final Button neutralButton = alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
+                        final Button positveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                        neutralButton.setTextColor(getResources().getColor(R.color.colorAccent));
+                        positveButton.setTextColor(getResources().getColor(R.color.colorAccent));
+
+
+                    }
+                });
 
             }
         };
+
+        if (adapter.getCount() == 0){
+            progress.hideProgress();
+            Snackbar.make(activity_main," Chat is empty...",Snackbar.LENGTH_SHORT).show();
+
+        }
         listOfMessage.setAdapter(adapter);
+
+
+
     }
+
+
+
 
 
 //    private void displayChatMessage() {
